659a4813b7619c45677ddbc34112fa5c81a87fd2
pranith <pashikantipranith7867@gmail.com>
2024-09-28 12:14:57 +0530

    updated api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,21 @@
 import serverless from 'serverless-http';
 import { app } from '../src/app';
 import ConnectDB from '../src/db/index';
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+});
+
+console.log(`process.env.PORT: ${process.env.PORT}`);
 
 // Initialize database connection
 ConnectDB();
+
+app.listen(process.env.PORT || 4005 , () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+
+});
 
 export const handler = serverless(app);
