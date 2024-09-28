b2b076cde41bcdd3e6c19b7d9e1e94c8e18eece5
pranith <pashikantipranith7867@gmail.com>
2024-09-28 12:57:09 +0530

    Added a home route

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,11 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieparser());
 
+// Root Route
+app.get('/', (_req, res) => {
+    res.send("Server is running fine");
+});
+
 // Routes
 import authRouter from "./routes/auth.route";
 import taskRouter from "./routes/task.route";
